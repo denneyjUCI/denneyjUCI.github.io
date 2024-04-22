a4b006c96c8def3dee7fdfc86533fc8c2477898b
Jonathan Denney <jonathan.denney1@gmail.com>
2024-04-22 10:25:37 -0500

    Update publishing pipeline

---
+++ Sources/DenneyjUCIGithubIo/main.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct DenneyjUCIGithubIo: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://denneyjuci.github.io/")!
+    var name = "Learn iOS with Jonathan"
+    var description = "A place where I can write for my future self"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try DenneyjUCIGithubIo().publish(using: [
+    .addMarkdownFiles(), // Import markdown files in the Content directory, parse and add them to PublishingContent
+    .copyResources(), // Add Resource content to Output
+    .generateHTML(withTheme:.foundation ), // Specify theme
+    .generateRSSFeed(including: [.posts]), // Generate RSS
+    .generateSiteMap(), // Generate Site Map
+    .unwrap(.gitHub("denneyjUCI/denneyjUCI.github.io", branch: "main", useSSH: true), PublishingStep.deploy)
+])
+
